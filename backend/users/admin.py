2afdcfc1638d7ff7bd17608f4a7fd63646d423db
Andrey Mirt <nrmously@gmail.com>
2024-12-26 02:54:16 +0300

    Fixes in users.

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -5,15 +5,27 @@ from django.contrib.auth.models import Group
 from users.models import Subscribers, User
 
 
+@admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    list_display = ('email', 'username', 'first_name', 'last_name', 'avatar')
+    list_display = ('email', 'username', 'first_name', 'last_name', 'avatar',
+                    'is_active')
     search_fields = ('email', 'username')
+    list_filter = ('is_active', 'date_joined')
+    list_per_page = 25
+    fields = ('email', 'username', 'first_name', 'last_name', 'avatar',
+              'is_active')
+    readonly_fields = ('date_joined',)
 
 
+@admin.register(Subscribers)
 class SubscribersAdmin(admin.ModelAdmin):
-    list_display = ('subscriber', 'subscribe_to')
+    list_display = ('subscriber', 'subscribe_to', 'created_at')
+    search_fields = ('subscriber__email', 'subscribe_to__email')
+    list_filter = ('subscribe_to',)
+    list_editable = ('subscribe_to',)
+    list_per_page = 20
+    fields = ('subscriber', 'subscribe_to', 'created_at')
+    readonly_fields = ('created_at',)
 
 
-admin.site.register(User, CustomUserAdmin)
-admin.site.register(Subscribers, SubscribersAdmin)
 admin.site.unregister(Group)
