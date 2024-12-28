000f1817c4408a5f3eb82ef5f70394a5829f9477
Andrey Mirt <nrmously@gmail.com>
2024-12-28 13:13:11 +0300

    flake8 fixes.

--- backend/users/models.py
+++ backend/users/models.py
@@ -28,6 +28,9 @@ class User(AbstractUser):
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
 
+    def __str__(self):
+        return self.email
+
 
 class Subscriber(models.Model):
     subscriber = models.ForeignKey(
